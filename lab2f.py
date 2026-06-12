165a6e60abf4ed21e52d87b733544a372dde1b2d
Pouya Mostafavi <pmostafavi@myseneca.ca>
2026-06-12 02:55:53 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Pouya Mostafavi
+# Author ID: Pmostafavi
+# Date Created: 2026/06/12
+
+import sys
+
+Timer = int(sys.argv[1])
+
+while Timer != 0:
+    print(Timer)
+    Timer = int(Timer) - 1
+
+if Timer == 0:
+    print('blast off!')
